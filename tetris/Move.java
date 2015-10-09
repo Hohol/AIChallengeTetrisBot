b154c18f6cd4a731381f51a9b8fd9c75089eef76
Hohol <nikita.glashenko@gmail.com>
2015-10-09 13:18:59 +0500

    Removed move keycodes

--- tetris/Move.java
+++ tetris/Move.java
@@ -1,17 +1,5 @@
 package tetris;
 
-import static java.awt.event.KeyEvent.*;
-
 public enum Move {
-    LEFT(VK_LEFT), RIGHT(VK_RIGHT), DROP(VK_SPACE), ROTATE_CW(VK_UP), ROTATE_CCW(VK_Z);
-
-    private final int keyCode;
-
-    Move(int keyCode) {
-        this.keyCode = keyCode;
-    }
-
-    public int getKeyCode() {
-        return keyCode;
-    }
+    LEFT, RIGHT, DROP, ROTATE_CW, ROTATE_CCW;
 }
