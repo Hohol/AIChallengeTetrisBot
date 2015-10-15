dc980c78aa5b68ad844e87aee868795ad8078578
Hohol <nikita.glashenko@gmail.com>
2015-10-15 23:54:36 +0500

    Messing with genetic algo

---
+++ nosubmit/Genetic/Chromosome.java
@@ -0,0 +1,11 @@
+package nosubmit.Genetic;
+
+import java.util.Random;
+
+public interface Chromosome<T extends Chromosome> {
+    T mutate(Random rnd);
+
+    T crossover(T other);
+
+    double getFitness(); // bigger fitness is better
+}
